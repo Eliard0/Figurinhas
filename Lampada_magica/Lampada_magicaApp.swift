d05c19b806aa1060a0623f03b8278fabb1ca78c6
Eliardo <eliardosilva81@gmail.com>
2024-09-27 15:11:06 -0300

    Initial Commit

---
+++ Lampada_magica/Lampada_magicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lampada_magicaApp.swift
+//  Lampada_magica
+//
+//  Created by Eliardo Venancio on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Lampada_magicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

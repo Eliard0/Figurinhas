a115fe43848053762625ade9be830b96654c1993
Eliardo <eliardosilva81@gmail.com>
2024-10-07 11:33:58 -0300

    lampada magica

--- Lampada_magica/ContentView.swift
+++ Lampada_magica/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var lampadaLigada: Bool = false
+    @State var lampadaPotencia: Double = 1.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Lampada Magica")
+                .font(.largeTitle)
+                .padding()
+            
+            Image(systemName: "lightbulb")
+                .font(.system(size: 300))
+            
+            Spacer()
+            
+            HStack {
+                Text("Lampada: ")
+                Toggle(lampadaLigada ? "Desligar" : "Ligar", isOn: $lampadaLigada)
+            }
+            
+            HStack {
+                Text("Potencia: ")
+                Slider(value: lampadaLigada ? $lampadaPotencia : .constant(0))
+                Text("\(lampadaLigada ? lampadaPotencia * 100 : 0.0, specifier: "%.0f")%")
+            }
+            
         }
         .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(lampadaLigada ? .yellow.opacity(lampadaPotencia) : .white)
     }
 }
 
